44e06ddc16737ace0b1541926f8b0978fc536c17
suprimregmi <suprim.regmi.5@gmail.com>
2026-01-21 23:18:04 +0545

    Add : rsp game oop_style added

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -50,8 +50,58 @@ else:
 ## Optimized:
 
 import random
-comps =  ['rock', 'paper', 'scissors']
-ai1 = random.choice(comps)
-ai2 = random.choice(comps)
-print(ai1,ai2)
+# class RockPaperScissors:
+#     def __init__(self):
+#         self.choices = ['rock', 'paper', 'scissors']
+#         self.rules = {'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper'}
+#     def get_choice(self):
+#         return random.choice(self.choices)
+#     def determine_winner(self,ai1,ai2):
+#         if ai1==ai2:
+#             return "It's a tie"
+#         elif self.rules[ai1] == ai2:
+#             return f" AI1 wins chose {ai1}"
+#         else:
+#             return f"AI2 wins chose{ai2}"
+#     def play(self):
+#         ai1 = self.get_choice()
+#         ai2 = self.get_choice()
+#     print("AI1: ", ai1)
+#     print("AI2", ai2)
+#     print(self.determine_winner(ai1,ai2))
+# if __name__ == '__main__':
+#     game = RockPaperScissors()
+#     game.play()
+
+
+
+
+import random
+class RockPaperScissors:
+    def __init__(self):
+        self.choices = ['rock', 'paper', 'scissors']
+        self.rules = {
+            'rock': 'scissors',
+            'scissors': 'paper',
+            'paper': 'rock'
+        }
+    def get_choice(self):
+        return random.choice(self.choices)
+
+    def determine_winner(self, ai1, ai2):
+        if ai1 == ai2:
+            return "It's a tie"
+        elif self.rules[ai1] == ai2:
+            return f"AI1 wins, chose {ai1}"
+        else:
+            return f"AI2 wins, chose {ai2}"
+    def play(self):
+        ai1 = self.get_choice()
+        ai2 = self.get_choice()
+        print("AI1:", ai1)
+        print("AI2:", ai2)
+        print(self.determine_winner(ai1, ai2))
+if __name__ == "__main__":
+    game = RockPaperScissors()
+    game.play()
 
